955321c52635edb57570c361431e279e87b5ac03
David Eastmond <davebuck1981@gmail.com>
2025-02-16 09:38:41 -0500

    create posting widget and user post components

---
+++ app/components/user-post/userpost.vue
@@ -0,0 +1,72 @@
+<template>
+  <!--This is the main post, designed to be rendered  -->
+  <div class="bg-smoke-grey p-4 rounded-md">
+    <header>
+      <div class="flex gap-2">
+        <Avatar :avatarUrl="avatarUrl" size="lg" />
+        <div>
+          <p>{{ userName }}</p>
+          <p class="font-thin text-sm">{{ postDate }}</p>
+        </div>
+      </div>
+    </header>
+    <div class="mt-4">
+      <p>{{ textContent }}</p>
+    </div>
+    <div class="flex justify-between mt-4 border-t p-2">
+      <!-- Reaction and comment section -->
+      <div>
+        <div v-if="reactionPanelVisible" class="absolute mt-[-70px]" v-on:mouseleave="togglePanelIfVisible()">
+          <Reactionpanel :post-id="id" v-on:reactionClicked="handleReactionClicked" />
+        </div>
+        <button class="min-w-[200px] hover:cursor-pointer" v-on:mouseover="togglePanelVisible()"
+          @click="togglePanelVisible()">React</button>
+      </div>
+      <div>
+        <button class="min-w-[200px] hover:cursor-pointer">Comment</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import type { Reaction } from '~/lib/types/user-posts/reaction';
+import Reactionpanel from '../reaction-panel/reactionpanel.vue';
+
+const { reactToPost } = usePost();
+const reactionPanelVisible = ref(false);
+type UserPostProps = {
+  avatarUrl: string;
+  userName: string;
+  postDate?: string;
+  textContent: string;
+  id: string;
+}
+/* As props we need
+- user avatar
+- user name
+- date-time ago
+- text content
+
+- TBD: multi media content
+*/
+const { avatarUrl, userName, postDate, textContent, id } = defineProps<UserPostProps>();
+
+function togglePanelVisible() {
+  reactionPanelVisible.value = !reactionPanelVisible.value;
+}
+
+function togglePanelIfVisible() {
+  if (reactionPanelVisible.value) {
+    reactionPanelVisible.value = false;
+  }
+}
+
+async function handleReactionClicked(reaction: Reaction) {
+  togglePanelIfVisible();
+  try {
+    await reactToPost(id, reaction);
+  } catch (error) {
+    console.error((error as Error).message);
+  }
+}
+</script>
