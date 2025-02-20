57b2d7e056aaed8243108d858c772277873a891f
David Eastmond <davebuck1981@gmail.com>
2025-02-20 17:55:04 -0500

    add some methods to use-user composable

--- composables/use-user.ts
+++ composables/use-user.ts
@@ -44,7 +44,46 @@ export function useUser() {
     const res = await $fetch<{ status: number; posts: UserPost[] }>(
       `/api/user/me/posts?limit=${limit}&skip=${skip}`
     );
+
+    console.log("48 posts", res);
     return res.posts;
+  }
+
+  async function getPostsByUserId({
+    userId,
+    limit,
+    skip,
+  }: {
+    userId: string;
+    limit?: number;
+    skip?: number;
+  }): Promise<UserPost[]> {
+    // Gets the posts of a user by their ID
+    const res = await $fetch<{ status: number; posts: UserPost[] }>(
+      `/api/user/${userId}/posts?limit=${limit}&skip=${skip}`
+    );
+    return res.posts;
+  }
+
+  async function getMyFollowers(): Promise<Partial<User>[]> {
+    const res = await $fetch<{ status: number; data: Partial<User>[] }>(
+      `/api/user/me/following`
+    );
+    return res.data;
+  }
+
+  async function followUser(userId: string) {
+    await $fetch<void>(`/api/user/me/following`, {
+      method: "POST",
+      body: { userId, action: "follow" },
+    });
+  }
+
+  async function unfollowUser(userId: string) {
+    await $fetch<void>(`/api/user/me/following`, {
+      method: "POST",
+      body: { userId, action: "unfollow" },
+    });
   }
 
   return {
@@ -52,5 +91,9 @@ export function useUser() {
     updateUserDetails,
     createPost,
     getMyPosts,
+    getPostsByUserId,
+    getMyFollowers,
+    followUser,
+    unfollowUser,
   };
 }
