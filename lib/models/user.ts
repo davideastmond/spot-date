b346c5d5921291c6677b69e7eb176505ad766041
David Eastmond <davebuck1981@gmail.com>
2025-02-21 08:42:41 -0500

    added some more utility user types

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -15,3 +15,12 @@ export type User = BaseUser & {
 };
 
 export type UpdatableUser = Pick<User, "nickname" | "bio">;
+
+// This type just removes sensitive information from the user object
+export type SecureUser = Omit<User, "email" | "spotifyUserId">;
+
+// This type is for security when sending data over to Angolia API
+export type SecureThirdPartyUser = Pick<
+  SecureUser,
+  "nickname" | "name" | "image"
+> & { objectID: string; userId: string };
