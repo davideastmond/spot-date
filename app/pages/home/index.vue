f9516a8fbd4382682d15e8a34f0d7a702e9e1d6a
David Eastmond <davebuck1981@gmail.com>
2025-02-27 17:21:08 -0500

    update reactions request handling

--- app/pages/home/index.vue
+++ app/pages/home/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <Feedheader v-if="posts.length > 0" :posts="posts" v-on:post-created="handlePostCreated" />
+  <Feedheader v-if="posts.length > 0" :posts="posts" v-on:post-created="handlePostCreated"
+    v-on:reaction-clicked="handleReactionClicked" />
 </template>
 <script setup lang="ts">
 import type { UserPost } from '~/lib/models/user-post';
+import type { Reaction } from '~/lib/types/user-posts/reaction';
 
 const posts = ref<UserPost[]>([]);
 const { getFeedByUserId } = useUser();
@@ -24,6 +26,13 @@ async function fetchPosts() {
   }
 }
 
+async function handleReactionClicked(postId: string, reaction: Reaction) {
+  try {
+    await fetchPosts();
+  } catch (error) {
+    console.error((error as Error).message);
+  }
+}
 async function handlePostCreated(data: string) {
   try {
     await createPost({
--- app/pages/home/me/index.vue
+++ app/pages/home/me/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <Feedheader v-if="posts.length > 0" :posts="posts" v-on:post-created="handlePostCreated" />
+  <Feedheader v-if="posts.length > 0" :posts="posts" v-on:post-created="handlePostCreated"
+    v-on:reaction-clicked="handleReactionClicked" />
 </template>
 
 <script setup lang="ts">
 import Feedheader from '~/app/components/feedheader/feedheader.vue';
 import type { UserPost } from '~/lib/models/user-post';
+import type { Reaction } from '~/lib/types/user-posts/reaction';
 
 const posts = ref<UserPost[]>([]);
 
@@ -26,6 +28,14 @@ const handlePostCreated = async (data: string) => {
   })
   await fetchPosts({});
 };
+
+async function handleReactionClicked(postId: string, reaction: Reaction) {
+  try {
+    await fetchPosts({});
+  } catch (error) {
+    console.error((error as Error).message);
+  }
+}
 
 const fetchPosts = async ({ limit, skip }: { limit?: number; skip?: number }) => {
   try {
