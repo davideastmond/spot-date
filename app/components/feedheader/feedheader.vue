6228a10aafd9e15c65590c4453d1c0eb93b7509d
David Eastmond <davebuck1981@gmail.com>
2025-03-05 14:30:26 -0500

    feed header component handles comment posts

--- app/components/feedheader/feedheader.vue
+++ app/components/feedheader/feedheader.vue
@@ -6,18 +6,21 @@
     <div class="flex flex-col gap-y-4 my-10" v-if="posts.length > 0 && avatarDict">
       <!-- Render posts here, figure out how to do scroll rendering -->
       <Userpost v-for="post in posts" :key="post.id" :post="post" :avatar-dict="avatarDict"
-        v-on:reaction-clicked="handleReactionClicked" />
+        v-on:reaction-clicked="handleReactionClicked" v-on:comment-created="handleCommentCreated" />
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import type { UserPost } from '~/lib/models/user-post';
+import type { UserCommentData } from '~/lib/types/user-posts/comments/user-comment-data';
 import type { ChosenMedia } from '~/lib/types/user-posts/media';
 import type { Reaction } from '~/lib/types/user-posts/reaction';
-const { onPostCreated, posts, onReactionClicked } = defineProps<{
-  onPostCreated: ({ postText, mediaContent }: { postText: string, mediaContent: ChosenMedia | null }) => void;
+
+const { onPostCreated, posts, onReactionClicked, onCommentCreated } = defineProps<{
+  onPostCreated: ({ postText, mediaContent }: { postText: string, mediaContent?: ChosenMedia | null }) => void;
   posts: UserPost[];
   onReactionClicked?: (postId: string, reaction: Reaction) => void;
+  onCommentCreated?: (data: UserCommentData) => void;
 }>();
 const avatarDict = ref<Record<string, { image: string | null | undefined, name: string, nickname: string }> | null>(null);
 const { getAvatarDict } = useUser();
@@ -29,5 +32,9 @@ onMounted(async () => {
 });
 function handleReactionClicked(postId: string, reaction: Reaction) {
   onReactionClicked?.(postId, reaction);
+}
+
+function handleCommentCreated(data: UserCommentData) {
+  onCommentCreated?.({ ...data });
 }
 </script>
