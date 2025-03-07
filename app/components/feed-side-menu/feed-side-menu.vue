2d3e38ac743214b9b53bbc865d81b5714d841f97
David Eastmond <davebuck1981@gmail.com>
2025-03-07 17:43:21 -0500

    icons

---
+++ app/components/feed-side-menu/feed-side-menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- This control is a side-bar that appears in large screen mode. -->
+
+  <div class="bg-smoke-grey invisible lg:visible w-[300px] max-h-fit p-4">
+    <ul class="p-4">
+      <slot name="avatar">
+
+      </slot>
+      <slot name="connections">
+
+      </slot>
+      <slot name="public-profile">
+
+      </slot>
+    </ul>
+  </div>
+</template>
+<script setup lang="ts">
+const { session } = useAuth();
+</script>
---
+++ app/components/notification-icon/notification-icon.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col">
+    <div class="rotate-[-20deg]">
+      <button>
+        <Icon name="material-symbols:notifications-sharp" width="24" height="24" />
+      </button>
+    </div>
+  </div>
+</template>
