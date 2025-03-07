4176d7ec574debc782a8bc9639e3b7b63fb5e8fd
David Eastmond <davebuck1981@gmail.com>
2025-03-07 07:36:26 -0500

    refactor the avatarDict

--- composables/use-user.ts
+++ composables/use-user.ts
@@ -82,27 +82,25 @@ export function useUser() {
     return res.posts;
   }
 
-  async function getAvatarDict(
-    userIds: string[]
-  ): Promise<
+  async function getAvatarDict(): Promise<
     Record<string, { image: string; name: string; nickname: string }>
   > {
-    const res = await Promise.allSettled(
-      userIds.map((userId) => getUserById(userId))
-    );
+    const res = await $fetch<{
+      data: { image: string; name: string; nickname: string; id: string }[];
+    }>("/api/user", {
+      method: "GET",
+    });
     const avatarDict: Record<
       string,
       { image: string; name: string; nickname: string }
     > = {};
 
-    res.forEach((result, index) => {
-      if (result.status === "fulfilled") {
-        avatarDict[userIds[index]] = {
-          image: result.value.image!,
-          name: result.value.name!,
-          nickname: result.value.nickname!,
-        };
-      }
+    res.data.forEach((result, index) => {
+      avatarDict[result.id] = {
+        image: result.image!,
+        name: result.name!,
+        nickname: result.nickname!,
+      };
     });
     return avatarDict;
   }
--- server/api/user/index.get.ts
+++ server/api/user/index.get.ts
@@ -1,11 +1,29 @@
+import { getServerSession } from "#auth";
+import { authOptions } from "~/lib/auth/auth";
 import { UserController } from "~/lib/controllers/user.controller";
-
 export default defineEventHandler(async (event) => {
+  const serverSession = await getServerSession(event, authOptions);
+
+  if (!serverSession || !serverSession.user) {
+    return {
+      statusCode: 401,
+      error: "Unauthorized request",
+    };
+  }
   try {
-    const user = await UserController.getAllUsers();
+    const users = await UserController.getAllUsers();
+
+    const sanitizedData = users.map((user) => {
+      return {
+        name: user.name,
+        nickname: user.nickname,
+        id: user.id,
+        image: user.image,
+      };
+    });
     return {
       statusCode: 200,
-      data: user,
+      data: sanitizedData,
     };
   } catch (error) {
     console.error(error);
