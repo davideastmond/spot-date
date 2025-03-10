915594038413c7f4d432635148094a41b2a967ca
David Eastmond <davebuck1981@gmail.com>
2025-03-10 17:34:32 -0400

    cleanup side menu

--- app/components/feed-side-menu/feed-side-menu.vue
+++ app/components/feed-side-menu/feed-side-menu.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- This control is a side-bar that appears in large screen mode. -->
 
-  <div class="bg-smoke-grey invisible lg:visible w-[300px] max-h-fit p-4">
+  <div class="bg-smoke-grey invisible lg:visible p-4">
     <ul class="p-4">
       <slot name="avatar">
 
