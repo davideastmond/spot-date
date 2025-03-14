cc5b719c575891726ad03614ebf831dc80f49d13
David Eastmond <davebuck1981@gmail.com>
2025-03-14 16:55:19 -0400

    fix

--- app/pages/profile/index.vue
+++ app/pages/profile/index.vue
@@ -78,7 +78,6 @@
         <textarea id="bio" maxlength="200" v-model="bio"
           class="resize-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Bio" required></textarea>
-
       </div>
       <div class="mt-6 flex justify-end">
         <button
