e7ab2c60c4e763a479fa939f6eb4eab3e4ef0173
David Eastmond <davebuck1981@gmail.com>
2025-03-26 10:08:06 -0400

    fixed stylings and truncates for notifications

--- app/components/notifications/notification-element.vue
+++ app/components/notifications/notification-element.vue
@@ -3,18 +3,22 @@
     <slot name="avatar"></slot>
     <NuxtLink v-if="notification.data?.link" :to="getNotificationUrl(notification.data.link)"
       @click="toggleNotificationsOpen()">
-      <div class="content-center max-w-[300px] truncatable">
+      <div class="content-center max-w-[300px]">
         <button @click="onNotificationElementClicked?.(notification.id!)">
           <li :key="notification.id" v-click-outside="toggleNotificationsOpen">{{
             notification.data.title }}</li>
-          <li v-if="notification.data?.body" class="text-xs">{{ notification.data.body }}</li>
+          <div class="truncatable max-h-[200px] max-w-[300px]">
+            <li v-if="notification.data?.body" class="text-xs">{{ notification.data.body }}</li>
+          </div>
         </button>
       </div>
     </NuxtLink>
-    <div v-else class="content-center max-w-[300px] truncatable">
+    <div v-else class="content-center max-w-[300px]">
       <button @click="onNotificationElementClicked?.(notification.id!)">
         <li :key="notification.id" v-click-outside="toggleNotificationsOpen">{{ notification.data!.title }}</li>
-        <li v-if="notification.data?.body" class="text-xs">{{ notification.data.body }}</li>
+        <div class="truncatable max-h-[200px] max-w-[300px]">
+          <li v-if="notification.data?.body" class="text-xs">{{ notification.data.body }}</li>
+        </div>
       </button>
     </div>
   </div>
