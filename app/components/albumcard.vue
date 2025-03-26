cb3f63f0b434c3a529114f78bb8f5f75974fe3f0
David Eastmond <davebuck1981@gmail.com>
2025-03-26 08:02:45 -0400

    rename file

--- app/components/albumcard.vue
+++ app/components/albumcard.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script setup lang="ts">
-import type { SpotifyAlbumItem } from '~/lib/types/spotify/album/spotify-album.types';
+import type { SpotifyAlbumItem } from '~/lib/types/spotify/album/spotify-album-types';
 
 
 const { name, images, artists } = defineProps<Partial<SpotifyAlbumItem>>();
