98b3e5b15137e12adae4353730072d9568e2dc93
David Eastmond <davebuck1981@gmail.com>
2025-04-01 18:35:35 -0400

    refactor the getReactingUsersData

---
+++ app/components/display-reactions-widget/display-reactions-widget.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="flex flex-row">
+    <div v-for="(_, reaction) in reactionAggregation()" :key="reaction">
+      <div>
+        <button @click="toggleReactorList(reaction)">
+          <p>{{ getReactionIcon(reaction) }}</p>
+        </button>
+        <div v-if="reactorDataMap[reaction] && reactorListVisible[reaction]"
+          v-on:mouseleave="toggleReactorList(reaction)" class="bg-white p-2 absolute fade-in z-1">
+          <p v-for="reactorData in reactorDataMap[reaction]" class="text-spotty-black text-sm">
+            {{ reactorData.nickname }}
+          </p>
+        </div>
+      </div>
+    </div>
+    {{ reactions.length }}
+  </div>
+</template>
+<script setup lang="ts">
+// This component counts the number of reactions and displays the count.
+// It also displays the different types of reactions as icons.
+// As a stretch goal, we can display the names of the users who reacted. ✔️
+import type { UserPostReaction } from '~/lib/models/user-post-reaction';
+import { getReactionIcon } from '~/lib/types/user-posts/reaction';
+
+type DisplayReactionsWidgetProps = {
+  reactions: UserPostReaction[];
+}
+
+const { reactions } = defineProps<DisplayReactionsWidgetProps>();
+const reactorDataMap = ref<Record<string, Array<{ userId: string, nickname: string, image: string | null }>>>({});
+const reactorListVisible = ref<Record<string, boolean>>({
+  like: false,
+  love: false,
+  laugh: false,
+  wow: false,
+  music: false,
+  jam: false
+});
+
+
+const reactionAggregation = () => {
+  const reactionMap: Record<string, number> = {};
+  reactions.forEach(reaction => {
+    if (reactionMap[reaction.reaction]) {
+      reactionMap[reaction.reaction] += 1;
+    } else {
+      reactionMap[reaction.reaction] = 1;
+    }
+  });
+
+  return reactionMap;
+}
+
+onMounted(async () => {
+  await getReactingUsersData();
+});
+
+function toggleReactorList(reaction: string) {
+  reactorListVisible.value[reaction] = !reactorListVisible.value[reaction];
+}
+
+async function getReactingUsersData() {
+  const { getUserById } = useUser();
+  try {
+    const requests = await Promise.all(reactions.map((r) => getUserById(r.posterId)));
+
+    reactorDataMap.value = reactions.reduce((acc, currentElement) => {
+      if (!acc[currentElement.reaction]) {
+        acc[currentElement.reaction] = [];
+      }
+      const reactor = requests.find((user) => user.id === currentElement.posterId);
+      if (reactor) {
+        acc[currentElement.reaction].push({
+          userId: reactor.id as string,
+          nickname: reactor.nickname as string,
+          image: reactor.image as string
+        });
+      }
+      return acc;
+    }, {} as Record<string, Array<{ userId: string, nickname: string, image: string | null }>>);
+
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+  }
+
+}
+</script>
