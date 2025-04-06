ef8fc59ba5eee1f7d1a41793a47cfc2cc00cb904
David Eastmond <davebuck1981@gmail.com>
2025-04-06 16:16:11 -0400

    app name spelling fix camelCase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
   app: {
     head: {
-      title: "Spotdate",
+      title: "SpotDate",
       htmlAttrs: {
         lang: "en",
       },
