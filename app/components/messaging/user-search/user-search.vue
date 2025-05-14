94b70e33e57efa73cf8d3a7575ae1e676e321339
David Eastmond <davebuck1981@gmail.com>
2025-05-14 09:03:10 -0400

    user search component

--- app/components/messaging/user-search/user-search.vue
+++ app/components/messaging/user-search/user-search.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
+  <div class="p-4 bg-spotty-black">
     <div>
       <input type="text" placeholder="Search for users..." v-on:keyup="handleInitiateSearch"
         class="rounded-lg h-[36px] p-2 w-full bg-smoke-grey focus:outline-none" />
     </div>
-    <div>
+    <div v-if="userSearchResults?.users?.length > 0">
       <!-- Search results go here -->
+      <div v-for="user in userSearchResults.users" :key="user.userId"
+        class="flex items-center gap-2 p-2 hover:bg-smoke-grey-light cursor-pointer"
+        @click="() => handleUserSelected(user.userId as string)">
+        <img :src="avatarDict[user.userId as string]?.image" alt="User Avatar" class="w-10 h-10 rounded-full" />
+        <span>{{ user.name }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -15,18 +21,20 @@ import type { AvatarDict } from '~/lib/definitions/avatar-dict/avatar-dict';
 
 type UserSearchProps = {
   onUserSelected?: (userId: string) => void;
-  avatarDict?: AvatarDict
+  avatarDict: AvatarDict
+}
+const { onUserSelected, avatarDict } = defineProps<UserSearchProps>();
+const { performSearch, userSearchResults, clearSearch } = useSearch();
+
+async function handleInitiateSearch(event: KeyboardEvent) {
+  const searchString = (event.target as HTMLInputElement).value;
+  if (searchString.length < 3) return;
+
+  await performSearch(searchString);
 }
 
-const { performSearch, userSearchResults } = useSearch()
-
-const searchQuery = ref<string>("");
-async function handleInitiateSearch(event: KeyboardEvent) {
-  // console.log((event.target as HTMLInputElement)?.value);
-
-  const searchString = (event.target as HTMLInputElement).value;
-  await performSearch(searchString);
-
-  console.log("Search results:", userSearchResults.users);
+function handleUserSelected(userId: string) {
+  clearSearch();
+  onUserSelected?.(userId);
 }
 </script>
