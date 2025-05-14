340fa3dd30bc50ab99815c66583d9c90e319f63b
David Eastmond <davebuck1981@gmail.com>
2025-05-14 09:40:12 -0400

    spacing for the participants header

--- app/components/messaging/participants-header/participants-header.vue
+++ app/components/messaging/participants-header/participants-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-spotty-deep-brown p-4 flex justify-end">
+  <div class="bg-spotty-deep-brown p-4 flex justify-end gap-2">
     <Avatar v-for="userId in participants" :key="userId" :avatar-url="avatarDict[userId]?.image" size="md">
       <Icon name="mdi:account-circle" style="color: white" size="32px" />
     </Avatar>
