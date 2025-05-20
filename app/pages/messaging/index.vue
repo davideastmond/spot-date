97a0e1fc33bff98c9b20c934828b92a75a5050e4
David Eastmond <davebuck1981@gmail.com>
2025-05-20 15:21:04 -0400

    remove try-catch block

--- app/pages/messaging/index.vue
+++ app/pages/messaging/index.vue
@@ -7,24 +7,27 @@
       <div v-if="noContextSelected">
         <p>Select a message</p>
       </div>
-      <MessageContentPanel :avatar-dict="avatarDict" v-else
-        :dm-session="dmSessions.find((session) => session.id === selectedMessageSessionId)">
-        <template #participantsHeader v-if="avatarDict && participants.length > 0">
-          <ParticipantsHeader :avatarDict="avatarDict" :participants="participants" v-if="router.query.new" />
-        </template>
-        <template #addUserWidget>
-          <div class="flex justify-end">
-            <button class="bg-blue-500 text-white p-2 text-xs font-thin">Add User...</button>
-          </div>
-        </template>
-        <template #messageInput>
-          <div class="flex gap-2 pt-2">
-            <input type="text" placeholder="Type a message..." class="w-full p-2 rounded bg-smoke-grey text-white"
-              v-model="inputMessage" @keyup="handleKeyboardInitiatedDm" />
-            <button class="bg-blue-500 text-white p-2 rounded" @click="handleDm">Send</button>
-          </div>
-        </template>
-      </MessageContentPanel>
+      <div v-else class="hidden lg:block">
+        <MessageContentPanel :avatar-dict="avatarDict"
+          :dm-session="dmSessions.find((session) => session.id === selectedMessageSessionId)">
+          <template #participantsHeader v-if="avatarDict && participants.length > 0">
+            <ParticipantsHeader :avatarDict="avatarDict" :participants="participants" v-if="router.query.new" />
+          </template>
+          <template #addUserWidget>
+            <div class="flex justify-end">
+              <button class="bg-blue-500 text-white p-2 text-xs font-thin">Add User...</button>
+            </div>
+          </template>
+          <template #messageInput>
+            <div class="flex gap-2 pt-2">
+              <input type="text" placeholder="Type a message..." class="w-full p-2 rounded bg-smoke-grey text-white"
+                v-model="inputMessage" @keyup="handleKeyboardInitiatedDm" />
+              <button class="bg-blue-500 text-white p-2 rounded" @click="handleDm">Send</button>
+            </div>
+          </template>
+        </MessageContentPanel>
+
+      </div>
     </div>
   </div>
   <Modal v-if="useSearchOpen" @close="closeUserSearch">
@@ -112,23 +115,18 @@ async function createNewDm() {
 
 async function createDmForCurrentSession() {
   if (!selectedMessageSessionId.value) return;
-  try {
-    await sendMessageBySessionId({
-      sessionId: selectedMessageSessionId.value as string,
-      text: inputMessage.value,
-      receiverIds: participants.value,
-    });
-    await getAllSessions();
-    inputMessage.value = "";
-  } catch (error) {
-    console.error("Error creating DM:", error);
-  }
-  // This is for existing messages, which send a message to an existing DM session.
+
+  await sendMessageBySessionId({
+    sessionId: selectedMessageSessionId.value as string,
+    text: inputMessage.value,
+    receiverIds: participants.value,
+  });
+  await getAllSessions();
+  inputMessage.value = "";
 }
 
 async function handleSessionContextChange(sessionId: string) {
   const dmSession = dmSessions.value.find((session) => session.id === sessionId);
-
 
   if (dmSession) {
     setMessageSessionId(sessionId);
